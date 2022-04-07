45de6f56fa306a9808184e5cae55206124f848bb
RASPBERRY <mivanov373@gmail.com>
2022-04-07 15:37:16 +0100

    Made adding quests simple

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ from Game_Classes.STATUS import STATUS
 class GameTests(unittest.TestCase):
     def test_adding_quests(self):
         utils = UTILITIES()
-        self.assertEqual(utils.addQuest(0xFAF, "Some objective", "Some description", STATUS.DOING), 1)
+        self.assertEqual(utils.addQuest(11, "Some objective", "Some description", STATUS.DOING), 1)
 
 
 if __name__ == '__main__':
